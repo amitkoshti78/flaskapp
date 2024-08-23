029196d052572e7584502b4db242c850d34d04e4
Amit Koshti <amitlkoshti@gmail.com>
2024-08-23 16:49:53 +0200

    'modification'

--- dbconnect.py
+++ dbconnect.py
@@ -5,28 +5,38 @@ conn = None
 
 def db_query(criteria, payload):
     global conn
+
     try:
         if conn is not None:
             # declare a cursor object from the connection
             cursor = conn.cursor()
             # print ("cursor object:", cursor, "\n")
-    except psycopg2.OperationalError:
+    except psycopg2.OperationalError as operr1:
         print("Print cursor state invalid")
-        cursor.close()
+        msg = f'Print cursor state invalid {operr1}'
     else:
         print("Cusrsor opened")
 
     try:
         sql_query = f'SELECT * FROM Customer where {criteria} = {payload};'
         cursor.execute(sql_query)
-    except psycopg2.OperationalError:
+    except psycopg2.OperationalError as operr2:
         print("SELECT statement failed")
+        msg = f'SELECT statement failed {operr2}'
     else:
         rows = cursor.fetchall()
         print(rows)
+        if cursor.rowcount() != 0:
+            msg = rows
+        else:
+            msg = f'Record not found'
+
     finally:
         print("Cursor closed")
         cursor.close()
+
+    return msg
+
 
 
 def db_connect(criteria, payload):
@@ -38,17 +48,22 @@ def db_connect(criteria, payload):
                                 password="Admin",
                                 port=5432)
 
-    except psycopg2.OperationalError:
+    except psycopg2.OperationalError as operr3:
         print("Data base connection error")
+        msg = f'Data base connection error {operr3}'
         conn = None
-    except psycopg2.DatabaseError:
+    except psycopg2.DatabaseError as dberr:
         print("Data base connection error")
+        msg = f'Data base connection error {dberr}'
         conn = None
     else:
         print("Database connected successfully")
-        db_query(criteria, payload)
+        msg = db_query(criteria, payload)
     finally:
         print("Data base closed")
         if conn is not None:
             conn.close()
+
+        return msg
+
 
